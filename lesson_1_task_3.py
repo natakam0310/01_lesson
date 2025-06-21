87bd650f9d5bd3d2a4cb7761ab17c565f33eb6a1
PodkovyrovaN <natakam0310@gmail.com>
2025-06-21 23:10:14 +0500

    first commit1

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,4 +1,3 @@
-first_name = input("Натали")
-last_name = input("Подковырова")
-
-print("Вас зовут: last_name first_name")
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+print(f"Вас зовут: {last_name} {first_name}")
